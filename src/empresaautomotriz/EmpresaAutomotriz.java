44fe7e058782a579e4f2a862417552a5906db332
josselyne <>
2020-12-06 18:43:51 -0500

    inicio del proyecto

---
+++ src/empresaautomotriz/EmpresaAutomotriz.java
@@ -0,0 +1,28 @@
+/*
+ * 13.	Una empresa automotriz necesita un sistema para seleccionar
+el tipo de carro (auto, SUV o camioneta) lo cual debe de aparecer 
+en un menú, y el color (negro, blanco o rojo) en otro menú. 
+Al final se necesita que despliegue la selección realizada. 
+Nota. Debe de anidarse una estructura de selección múltiple dentro de otra.
+ */
+package empresaautomotriz;
+
+import java.util.Scanner;
+
+/**
+ *josselyne ester chilito galindez 
+ */
+public class EmpresaAutomotriz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Scanner entrada=new Scanner(System.in);
+       
+       swicht({
+        
+    }
+    }
+    
+}
