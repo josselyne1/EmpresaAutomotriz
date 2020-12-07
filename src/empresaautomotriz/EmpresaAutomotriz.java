f869fbb36393c5588b1dc91f6d2e92a0b2ef95c9
josselyne <USUARIO@DESKTOP-KOEI58B>
2020-12-06 20:46:46 -0500

    final del proyecto

--- src/empresaautomotriz/EmpresaAutomotriz.java
+++ src/empresaautomotriz/EmpresaAutomotriz.java
@@ -20,9 +20,122 @@ public class EmpresaAutomotriz {
     public static void main(String[] args) {
        Scanner entrada=new Scanner(System.in);
        
-       swicht({
+        System.out.println("eliga el tipo de carro");
+        System.out.println("______________________");
+        System.out.println("1. para carro");
+        System.out.println("2. para suv");
+        System.out.println("3. caminoneta");
+        System.out.println("________________________");
         
-    }
-    }
+        int j1,j2;//variables para los switch
+        String tipoA="a", color="a";//tipo de dato de cadena, no primitivo
+        j1=entrada.nextInt();
+        
+        switch (j1){
+            case 1: 
+                tipoA="carro";
+                System.out.println("elija el color del carro");
+                System.out.println("________________________");
+                System.out.println("1 para negro");
+                System.out.println("2 para rojo ");
+                System.out.println("para blanco");
+                
+                j2=entrada.nextInt();
+                switch (j2){
+                    case 1:
+                        color ="negro";
+                        break;
+                    case 2:
+                        color="rojo";
+                        break;
+                    case 3:
+                        color="blanco";
+                    default://de otro modo
+                        System.out.println("selecciono un numero no adecuado");
+                }                      
+               break;//de otro modo
+            case 2: 
+                tipoA="suv";
+                System.out.println("elija el color de la suv");
+                System.out.println("________________________");
+                System.out.println("1 para negro");
+                System.out.println("2 para rojo ");
+                System.out.println("para blanco");
+                
+                j2=entrada.nextInt();
+                switch (j2){
+                    case 1:
+                        color="negro";
+                        break;
+                    case 2:
+                        color="rojo";
+                        break;
+                    case 3:
+                        color="blanco";
+                    default:
+                        System.out.println("selecciono un numero no adecuado");
+                }        
+            case 3:
+                tipoA="camioneta";
+                System.out.println("elija el color de la camioneta");
+                System.out.println("________________________");
+                System.out.println("1 para negro");
+                System.out.println("2 para rojo ");
+                System.out.println("para blanco"); 
+                
+                j2=entrada.nextInt();
+                switch (j2){
+                   
+                    case 1:
+                        color="negro";
+                    case 2:
+                        color="rojo";
+                    case 3:
+                        color="blanco";
+                    default:
+                       System.out.println("selecciono un numero no adecuado");}
+        break;    
+       default:System.out.println("seleciono un numero no adecuado");}        
+        System.out.println("su tipo de auto es"+tipoA);
+        System.out.println("su color de auto es"+color);
+         
+            
+                
+                        
+                        
+                        
+                    
+                
+                    
+                    
+        
+    }         
+                
+                
+           
+                        
+               
+                
+               
+                
+                
+        
+                
+            
+            
+}   
+        
+       
+        
+        
+        
+        
+        
+         
+       
+        
+        
+    
+   
     
 }
